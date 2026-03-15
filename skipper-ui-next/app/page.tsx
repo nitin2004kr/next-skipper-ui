1db2722d346c197b00e33cdb02aa64c1b2dc516e
nitin2004kr <nitin112004kr@gmail.com>
2026-03-15 11:14:45 +0530

    created the navigaiton animation form skipper ui

--- skipper-ui-next/app/page.tsx
+++ skipper-ui-next/app/page.tsx
@@ -1,8 +1,9 @@
+import { Skiper58 } from "@/components/pages/Home";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     tgasfafd
+          <Skiper58 />
     </div>
   );
 }
